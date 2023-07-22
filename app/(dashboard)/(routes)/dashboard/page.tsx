1d2242a084b30dfbfd8a8a2006c5cdb9a95beb1f
Avinash Yadav <109527105+logan-14@users.noreply.github.com>
2023-07-22 18:58:37 +0530

    initial

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const DashboardPage = () => {
+  return (
+    <p>Dash board Page</p>
+  )
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const LandingPage = () => {
+  return (
+ <div>
+    Unp
+ </div>
+  )
+}
+
+export default LandingPage;
+
